e59d2cf86b61e646f0bda45659cc3db974c48c59
nabinchaulagain <wrighteric002@gmail.com>
2020-05-01 11:12:21 +0545

    running tests in band

--- tests/helpers/db.js
+++ tests/helpers/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 module.exports = {
   connect: () => {
-    return mongoose.connect(process.env.MONGO_URI + Date.now(), {
+    return mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true
     });
