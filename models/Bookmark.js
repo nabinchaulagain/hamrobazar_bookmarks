42ea32bd3bacf6e0e27075fe3b0fbae40665140c
nabinchaulagain <wrighteric002@gmail.com>
2020-05-27 21:42:44 +0545

    deleting notifications when deleting bookmarks

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const itemSchema = require("./Item.schema");
+const Notification = require("./Notification");
 const bookmarkSchema = new mongoose.Schema({
   name: {
     type: String
@@ -11,6 +12,11 @@ const bookmarkSchema = new mongoose.Schema({
   userId: mongoose.Types.ObjectId,
   criteria: { type: Object },
   latestItem: { type: itemSchema, select: false }
+});
+
+bookmarkSchema.pre("remove", async function (next) {
+  await Notification.deleteMany({ bookmark: this._id });
+  next();
 });
 
 const Bookmark = mongoose.model("Bookmark", bookmarkSchema);
